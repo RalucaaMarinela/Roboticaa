488dabca60f33a44a16e371290bd7617c55e5b55
RalucaaMarinela <48826322+RalucaaMarinela@users.noreply.github.com>
2019-11-22 16:42:43 +0200

    Add files via upload

--- Labhomework/Homework_2.ino
+++ Labhomework/Homework_2.ino
@@ -1,63 +1,157 @@
-const int knock = A0;
-int speakerValue = 0;
-int speakerValueLoop =0;
-int pushButton =0;
-int pinLed = 0;
-int button = 2;
-unsigned long timeBuzzer = 0;
-unsigned long timeCurrentBuzzer = 0;
-int buzzerTone = 1;
-const int threshold = 0;
-const int buzzerOutput = 10;
-int preButton = LOW;
-int actualButton = HIGH;
-int valueButton = 0;
+
+const int pinSw = A2; 
+const int pinX = A0; 
+const int pinY = A1; 
+
+const int pinA = 12;
+const int pinB = 8;
+const int pinC = 5;
+const int pinD = 3;
+const int pinE = 2;
+const int pinF = 11;
+const int pinG = 6;
+const int pinDP = 4;
+const int pinD1 = 7;
+const int pinD2 = 9;
+const int pinD3 = 10;
+const int pinD4 = 13;
+
+const int segSize = 8;
+const int noOfDisplays = 4;
+const int noOfDigits = 10;
+
+bool buttonState = true;
+int swState;
+int dpState = LOW;
+
+int switchValue;
+int xValue = 0;
+int yValue = 0;
+
+bool joyMoved = false;
+int digit =0;
+int minThreshold = 400;
+int maxThreshold = 600;
+int number =0;
+
+// segments array, similar to before
+int segments[segSize] = {
+  pinA, pinB, pinC, pinD, pinE, pinF, pinG, pinDP
+};
+// digits array, to switch between them easily
+int digits[noOfDisplays] = {
+  pinD1, pinD2, pinD3, pinD4
+ };   
+ 
+byte digitMatrix[noOfDigits][segSize - 1] = {
+// a  b  c  d  e  f  g
+  {1, 1, 1, 1, 1, 1, 0}, // 0
+  {0, 1, 1, 0, 0, 0, 0}, // 1
+  {1, 1, 0, 1, 1, 0, 1}, // 2
+  {1, 1, 1, 1, 0, 0, 1}, // 3
+  {0, 1, 1, 0, 0, 1, 1}, // 4
+  {1, 0, 1, 1, 0, 1, 1}, // 5
+  {1, 0, 1, 1, 1, 1, 1}, // 6
+  {1, 1, 1, 0, 0, 0, 0}, // 7
+  {1, 1, 1, 1, 1, 1, 1}, // 8
+  {1, 1, 1, 1, 0, 1, 1}  // 9
+};
 
 
-void setup() {
-   Serial.begin(9600);
-   pinMode(knock,INPUT);
-   pinMode(button, INPUT_PULLUP);
-   pinMode(buzzerOutput, OUTPUT);
-   speakerValue = analogRead(knock);
+void displayNumber(byte number, byte decimalPoint) {
+  for (int i = 0; i < segSize - 1; i++) {
+  digitalWrite(segments[i], digitMatrix[number][i]);
+  }
 
+  // write the decimalPoint to DP pin
+  digitalWrite(segments[segSize - 1], decimalPoint);
 }
 
+// activate the display no. received as param
+void showDigit(int num) {
+  for (int i = 0; i < noOfDisplays; i++) {
+  digitalWrite(digits[i], HIGH);
+  }
+  digitalWrite(digits[num], LOW);
+}
+         
 
-int buttonFunction ()
-{
-  actualButton = digitalRead(button);
-  if(preButton != actualButton)
-  return 1;
-  return 0;
+void setup() {
+  for (int i = 0; i < segSize - 1; i++)
+  {
+  pinMode(segments[i], OUTPUT);  
+  }
+  for (int i = 0; i < noOfDisplays; i++)
+  {
+  pinMode(digits[i], OUTPUT);  
+  }
+  pinMode(pinSw, INPUT_PULLUP);
+  displayNumber(number, dpState);
+  Serial.begin(9600);
 }
 
 void loop() {
 
-
-  speakerValueLoop = analogRead(knock); 
-  valueButton = buttonFunction(); 
- 
- 
-  noTone(buzzerOutput);
   
-  if( speakerValueLoop > speakerValue)
-  {
-   timeCurrentBuzzer = millis();
+  //showDigit(digit); 
+  swState = digitalRead(pinSw); 
+  Serial.println(swState);
+
+  if (swState == 1){
+   
+  xValue = analogRead(pinX);
+  showDigit(digit);
+  if(xValue < minThreshold && joyMoved == false){
+    if ( number > 0)
+    {
+      number--;
+    } else{
+      number = 9;
+    }
+    joyMoved = true;
+   
+   
   }
-  if(millis() >= timeCurrentBuzzer + 500)
-  {
-     tone(knock, buzzerTone, 5);
+  
+  if( xValue > maxThreshold && joyMoved == false){
+    if(number < 9){
+      number++;
+    }else {
+      number = 0;
+    }
+    joyMoved = true;
+  
+  }
+
+  if(xValue >= minThreshold && xValue <= maxThreshold){
+    joyMoved = false;
+  }
+
+  displayNumber(number, HIGH);
+  }
+  else{
     
-  
-
-  if(valueButton == 1)
-  {
-    noTone(buzzerOutput);
-  }
-  }
-
-
- 
- // Serial.println(timeBuzzer);
+    yValue = analogRead(pinY);
+    Serial.println(yValue);
+    if(yValue < minThreshold && joyMoved == false){
+      if(digit < 4){
+        digit++;
+      }else{
+        digit = 0;
+      }
+          showDigit(digit);
+       
+        joyMoved = true;
+      }
+       if(yValue > maxThreshold && joyMoved == false){
+      if(digit > 0){
+        digit--;
+      }else{
+        digit = 4;
+      }
+      showDigit(digit);
+      joyMoved = true;
+    }
+      
+  }  
 }
